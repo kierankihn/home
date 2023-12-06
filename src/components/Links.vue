f9f0eebf89768d549416a6abb6376d1fe375f323
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-12-06 09:25:17 +0800

    feat: updated link icon

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -7,26 +7,15 @@
       <span class="title">网站列表</span>
     </div>
     <!-- 网站列表 -->
-    <Swiper
-      v-if="siteLinks[0]"
-      :modules="[Pagination, Mousewheel]"
-      :slides-per-view="1"
-      :space-between="40"
-      :pagination="{
-        el: '.swiper-pagination',
-        clickable: true,
-        bulletElement: 'div',
-      }"
-      :mousewheel="true"
-    >
+    <Swiper v-if="siteLinks[0]" :modules="[Pagination, Mousewheel]" :slides-per-view="1" :space-between="40" :pagination="{
+      el: '.swiper-pagination',
+      clickable: true,
+      bulletElement: 'div',
+    }" :mousewheel="true">
       <SwiperSlide v-for="site in siteLinksList" :key="site">
         <el-row class="link-all" :gutter="20">
           <el-col v-for="(item, index) in site" :span="8" :key="item">
-            <div
-              class="item cards"
-              :style="index < 3 ? 'margin-bottom: 20px' : null"
-              @click="jumpLink(item)"
-            >
+            <div class="item cards" :style="index < 3 ? 'margin-bottom: 20px' : null" @click="jumpLink(item)">
               <Icon size="26">
                 <component :is="siteIcon[item.icon]" />
               </Icon>
@@ -43,8 +32,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Blog, Cloud, Mastodon, Code } from "@vicons/fa"; // 注意使用正确的类别
-import { MediaLibrary, Dashboard } from "@vicons/carbon";
+import { Blog, Cloud, Mastodon, Code, Film, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
@@ -69,11 +57,10 @@ const siteLinksList = computed(() => {
 const siteIcon = {
   Blog,
   Cloud,
-  CompactDisc,
-  Compass,
-  Book,
-  Fire,
-  LaptopCode,
+  Mastodon,
+  Code,
+  Film,
+  LaptopCode
 };
 
 // 链接跳转
@@ -98,37 +85,45 @@ onMounted(() => {
     display: flex;
     align-items: center;
     animation: fade 0.5s;
+
     .title {
       margin-left: 8px;
       font-size: 1.15rem;
       text-shadow: 0 0 5px #00000050;
     }
   }
+
   .swiper {
     left: -10px;
     width: calc(100% + 20px);
     padding: 5px 10px 0;
     z-index: 0;
+
     .swiper-slide {
       height: 100%;
     }
+
     .swiper-pagination {
       position: static;
       margin-top: 4px;
+
       :deep(.swiper-pagination-bullet) {
         background-color: #fff;
         width: 18px;
         height: 4px;
         border-radius: 4px;
         transition: opacity 0.3s;
+
         &:hover {
           opacity: 1;
         }
       }
     }
   }
+
   .link-all {
     height: 220px;
+
     .item {
       height: 100px;
       width: 100%;
@@ -153,16 +148,20 @@ onMounted(() => {
         font-size: 1.1rem;
         margin-left: 8px;
       }
+
       @media (min-width: 720px) and (max-width: 820px) {
         .name {
           display: none;
         }
       }
+
       @media (max-width: 720px) {
         height: 80px;
       }
+
       @media (max-width: 460px) {
         flex-direction: column;
+
         .name {
           font-size: 1rem;
           margin-left: 0;
@@ -170,6 +169,7 @@ onMounted(() => {
         }
       }
     }
+
     @media (max-width: 720px) {
       height: 180px;
     }
